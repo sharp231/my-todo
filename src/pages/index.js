533f60e547d766de9bbb629ceb95c8cbf18af2a4
Orvion <yuudai07ya@gmail.com>
2025-07-16 10:11:39 +0900

    💄

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,19 @@
 // import React, { useState } from 'react';
+import Head from 'next/head';
 import TodoApp from '../../components/TodoApp';
+
 
 const Home = () => {
   return (
     <div>
+      <Head>
+        <title>Todoアプリ</title>
+        <meta name="description" content="Next.js Todoアプリ" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1>Todoアプリ</h1>
       <TodoApp />
     </div>
   );
 }
 export default Home;
-//
